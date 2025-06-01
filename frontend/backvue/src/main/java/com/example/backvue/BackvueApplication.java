01761d93b918bc0d057b2ac29336f7bc9248ecf2
ClaudiaCBS <claudiacarlosbraga@gmail.com>
2025-06-01 03:48:14 -0300

    Correção no Backend Projeto Usuario

--- frontend/backvue/src/main/java/com/example/backvue/BackvueApplication.java
+++ frontend/backvue/src/main/java/com/example/backvue/BackvueApplication.java
@@ -1,13 +1,13 @@
-package br.gov.sp.fatec.springboot3top20251;
+package com.example.backvue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot3top20251Application {
+public class BackvueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot3top20251Application.class, args);
+		SpringApplication.run(BackvueApplication.class, args);
 	}
 
 }
--- frontend/backvue/src/test/java/com/example/backvue/BackvueApplicationTests.java
+++ frontend/backvue/src/test/java/com/example/backvue/BackvueApplicationTests.java
@@ -1,10 +1,10 @@
-package br.gov.sp.fatec.springboot3top20251;
+package com.example.backvue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Springboot3top20251ApplicationTests {
+class BackvueApplicationTests {
 
 	@Test
 	void contextLoads() {
